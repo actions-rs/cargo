03012abe8b8528f56cfeb043814775cb684d0e52
Alexander Lyon <arlyon@me.com>
2022-03-04 09:40:16 +0000

    Add stdout and stderr outputs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as core from "@actions/core";
 import * as input from "./input";
 import { Cargo, Cross } from "@actions-rs/core";
 
-export async function run(actionInput: input.Input): Promise<void> {
+export async function run(
+    actionInput: input.Input
+): Promise<{ code: number; stdout: string; stderr: string }> {
     let program;
     if (actionInput.useCross) {
         program = await Cross.getOrInstall();
@@ -20,7 +22,23 @@ export async function run(actionInput: input.Input): Promise<void> {
     args.push(actionInput.command);
     args = args.concat(actionInput.args);
 
-    await program.call(args);
+    let stdout = "";
+    let stderr = "";
+
+    const options = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                stdout += data.toString();
+            },
+            stderr: (data: Buffer) => {
+                stderr += data.toString();
+            },
+        },
+    };
+
+    const code = await program.call(args, options);
+
+    return { code, stdout, stderr };
 }
 
 async function main(): Promise<void> {
@@ -30,7 +48,9 @@ async function main(): Promise<void> {
     const actionInput = input.get();
 
     try {
-        await run(actionInput);
+        const { stdout, stderr } = await run(actionInput);
+        core.setOutput("stdout", stdout);
+        core.setOutput("stdout", stderr);
     } catch (error) {
         core.setFailed((<Error>error).message);
     }
